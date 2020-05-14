312b94ad60d4df71626cf5dfeac4a77cf9271181
akfoster2015 <akfoster2015@gmail.com>
2020-05-13 17:45:51 -0700

    backend users, contacts and JWT authentication

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API...' })
